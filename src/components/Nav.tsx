4e20895a7adea1540c64d0982183efb2ef13b6ed
sungg <zwsun1948@163.com>
2020-06-17 22:25:05 +0800

    完成底部导航

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,10 @@
-import styled from "styled-components";
-import React from "react";
-import { Link } from "react-router-dom";
+import styled from 'styled-components';
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+require('icons/tag.svg');
+require('icons/money.svg');
+require('icons/chart.svg');
 
 const NavWrapper = styled.div`
   box-shadow: 0 0 3px rgba(0,0,0,0.3);
@@ -9,26 +13,39 @@ const NavWrapper = styled.div`
     > li{
       width: 333333%;
       text-align: center;
-      padding: 16px;
+      padding: 4px;
+      display: flex;
+      flex-direction: column; 
+      align-items: center;
     }
   }
-`
-function Nav (){
+`;
+
+function Nav() {
   return (
     <NavWrapper>
       <ul>
         <li>
+          <svg className='icon'>
+            <use xlinkHref='#tag'/>
+          </svg>
           <Link to="/tag">标签</Link>
         </li>
         <li>
+          <svg className='icon'>
+            <use xlinkHref='#money'/>
+          </svg>
           <Link to="/money">记账</Link>
         </li>
         <li>
+          <svg className='icon'>
+            <use xlinkHref='#chart'/>
+          </svg>
           <Link to="/statistics">统计</Link>
         </li>
       </ul>
     </NavWrapper>
-  )
+  );
 }
 
 export default Nav;

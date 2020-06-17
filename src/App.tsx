b23dd38b05d676245e5d409df522742e214964c1
sungg <zwsun1948@163.com>
2020-06-17 17:21:30 +0800

    init Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,64 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div></div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/tag">标签</Link>
+            </li>
+            <li>
+              <Link to="/money">记账</Link>
+            </li>
+            <li>
+              <Link to="/statistics">统计</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/tag">
+            <Tag/>
+          </Route>
+          <Route path="/money">
+            <Money/>
+          </Route>
+          <Route path="/statistics">
+            <Statistics/>
+          </Route>
+          <Redirect exact from="/" to="/tag"/>
+          <Route path="*">
+            <NoMatch/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
+}
+
+function Statistics() {
+  return <h2>统计页面</h2>;
+}
+
+function Tag() {
+  return <h2>标签页面</h2>;
+}
+
+function Money() {
+  return <h2>记账页面</h2>;
+}
+
+function NoMatch() {
+  return <div>你输入的路径不存在</div>;
 }
 
 export default App;

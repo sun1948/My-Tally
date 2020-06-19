d665f545a452f3e625923247335d880d6d20f9ac
sungg <zwsun1948@163.com>
2020-06-19 15:44:16 +0800

    重构 NumberPad.tsx

--- src/views/Money/NumberPad.tsx
+++ src/views/Money/NumberPad.tsx
@@ -1,41 +1,7 @@
-import styled from 'styled-components';
 import React, {useState} from 'react';
+import {Wrapper} from './NumberPad/Wrapper';
+import {generateOutput} from './NumberPad/generateOutput';
 
-const Wrapper = styled.section`
-  output{
-    display: block;
-    font-size: 36px;
-    line-height: 72px;
-    text-align: end;
-    padding: 0 16px;
-    box-shadow: inset 0 -5px 5px -5px rgba(0,0,0,0.25);
-  }
-  .pad{
-    > button{
-      float: left;
-      width: 25%; font-size: 18px; line-height: 22px; height: 64px;
-      border: none;
-      &.ok{ height: 128px; background: #8B8B8B; float:right;  }
-      &:nth-child(1){
-        background: #F2F2F2;
-      }
-      &:nth-child(2), :nth-child(5){
-        background: #E0E0E0;
-      }
-      &:nth-child(3), :nth-child(6), :nth-child(9){
-        background: #D3D3D3;
-      }
-      &:nth-child(4), :nth-child(7), :nth-child(10){
-        background: #C1C1C1;
-      }
-      &:nth-child(8), :nth-child(11){
-        background: #A9A9A9;
-      }
-      &.dot{background: #9A9A9A;}
-      &.zero{ width: 50%; background: #A9A9A9;}
-    }
-  }
-`;
 
 const NumberPadSection: React.FC = () => {
   const [output, _setOutput] = useState('0');
@@ -50,42 +16,13 @@ const NumberPadSection: React.FC = () => {
   const onClick = (e: React.MouseEvent) => {
     const text = (e.target as HTMLButtonElement).textContent;
     if (text === null) return;
-    switch (text) {
-      case '0':
-      case '1':
-      case '2':
-      case '3':
-      case '4':
-      case '5':
-      case '6':
-      case '7':
-      case '8':
-      case '9':
-        if (output === '0') {
-          setOutput(text);
-        } else {
-          setOutput(output + text);
-        }
-        break;
-      case '.':
-        if (output.indexOf('.') >= 0) {return;}
-        setOutput(output + '.');
-        break;
-      case '删除':
-        if (output.length === 1) {
-          setOutput('');
-        } else {
-          setOutput(output.slice(0, -1));
-        }
-        break;
-      case '清空':
-        setOutput('');
-        break;
-      case 'OK':
-        console.log('ok');
-        break;
+    if(text === 'ok'){
+      //TODO
+      return
     }
+    setOutput(generateOutput(text,output));
   };
+
   return (
     <Wrapper>
       <output>{output}</output>
@@ -101,7 +38,7 @@ const NumberPadSection: React.FC = () => {
         <button>7</button>
         <button>8</button>
         <button>9</button>
-        <button className="ok">OK</button>
+        <button className="ok">ok</button>
         <button className="zero">0</button>
         <button className="dot">.</button>
       </div>

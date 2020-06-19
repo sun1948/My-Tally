94c98b4bd2f5a50e83d41e78233f1ac957758907
sungg <zwsun1948@163.com>
2020-06-19 14:34:05 +0800

    数字面板功能基本完成

--- src/views/Money/NumberPad.tsx
+++ src/views/Money/NumberPad.tsx
@@ -1,5 +1,5 @@
-import styled from "styled-components";
-import React, { useState } from "react";
+import styled from 'styled-components';
+import React, {useState} from 'react';
 
 const Wrapper = styled.section`
   output{
@@ -37,11 +37,19 @@ const Wrapper = styled.section`
   }
 `;
 
-const NumberPadSection:React.FC = ()=>{
-  const [output,setOutput] = useState('0');
-  const onClick = (e:React.MouseEvent)=>{
+const NumberPadSection: React.FC = () => {
+  const [output, _setOutput] = useState('0');
+  const setOutput = (output: string) => {
+    if (output.length > 16) {
+      output = output.slice(0,16);
+    }else if(output.length === 0){
+      output = '0';
+    }
+    _setOutput(output);
+  };
+  const onClick = (e: React.MouseEvent) => {
     const text = (e.target as HTMLButtonElement).textContent;
-    if(text === null)return;
+    if (text === null) return;
     switch (text) {
       case '0':
       case '1':
@@ -53,25 +61,32 @@ const NumberPadSection:React.FC = ()=>{
       case '7':
       case '8':
       case '9':
-      case '.':
-        if(output === '0'){
+        if (output === '0') {
           setOutput(text);
-        }else{
-          setOutput(output+text)
+        } else {
+          setOutput(output + text);
         }
         break;
+      case '.':
+        if (output.indexOf('.') >= 0) {return;}
+        setOutput(output + '.');
+        break;
       case '删除':
-        console.log('删除');
+        if (output.length === 1) {
+          setOutput('');
+        } else {
+          setOutput(output.slice(0, -1));
+        }
         break;
       case '清空':
-        console.log('清空');
+        setOutput('');
         break;
       case 'OK':
         console.log('ok');
         break;
     }
-  }
-  return(
+  };
+  return (
     <Wrapper>
       <output>{output}</output>
       <div className="pad clearfix" onClick={onClick}>
@@ -91,6 +106,6 @@ const NumberPadSection:React.FC = ()=>{
         <button className="dot">.</button>
       </div>
     </Wrapper>
-  )
-}
+  );
+};
 export {NumberPadSection};

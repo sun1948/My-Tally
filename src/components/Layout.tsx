77936b2123e37bb13331a0ae619cce54d877f511
sungg <zwsun1948@163.com>
2020-06-21 20:39:03 +0800

    lint

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 import React from 'react';
 
 const Wrapper = styled.div`
-  border: 1px solid black;
   display: flex;
   flex-direction: column;
   height:100vh;

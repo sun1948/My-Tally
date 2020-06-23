ba5de8be936400076f9cc31e6c7c3adcf2c1094a
sungg <zwsun1948@163.com>
2020-06-23 11:06:04 +0800

    改写 updateTag

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -10,20 +10,13 @@ const defaultTags = [
 const useTags = () => {   //封装自定义hook，即一个函数内部使用useState/useRef等等，然后返回一个接口。
   const [tags, setTags] = useState<{ id: number; name: string }[]>(defaultTags);
   const findTag = (id: number) => tags.filter(tag => tag.id === id)[0];
-  const findTagIndex = (id: number) => {
-    let result = -1;
-    for (let i = 0; i < tags.length; i++) {
-      if (tags[i].id === id) {
-        result = i;
-        break;
-      }
-    }
-    return result;
-  };
-  const tagsClone = JSON.parse(JSON.stringify(tags));   //深拷贝
-  const updateTag = (id: number, obj: { name: string }) => {
-    tagsClone.splice(findTagIndex(id), 1, {id: id, name: obj.name});
-    setTags(tagsClone);
+  const updateTag = (id: number, {name}: { name: string }) => {
+    // 深拷贝写法
+    // tagsClone.splice(findTagIndex(id), 1, {id: id, name: obj.name});
+    // setTags(tagsClone);
+    setTags(tags.map(tag =>
+      tag.id === id ? {id, name} : tag
+    ));
   };
   const deleteTag = (id: number) => {
     setTags(tags.filter(tag => tag.id !== id));
@@ -32,3 +25,15 @@ const useTags = () => {   //封装自定义hook，即一个函数内部使用use
 };
 
 export {useTags};
+
+// const findTagIndex = (id: number) => {
+//   let result = -1;
+//   for (let i = 0; i < tags.length; i++) {
+//     if (tags[i].id === id) {
+//       result = i;
+//       break;
+//     }
+//   }
+//   return result;
+// };
+// const tagsClone = JSON.parse(JSON.stringify(tags));

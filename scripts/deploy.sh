b9706482cfbb027284ce3b1f0f85e349ec80cb84
sungg <zwsun1948@163.com>
2020-06-26 14:08:09 +0800

    add script

---
+++ scripts/deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+yarn build &&
+cd build &&
+git init &&
+ga . &&
+git commit -m 'deploy' &&
+git remote add origin git@github.com:sun1948/MyTally-website.git &&
+git push -u origin master -f
+cd -
+
